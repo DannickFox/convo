abbb6f93df24d735e7e725155ff16b45d4b4b1aa
Daniel Dominguez <dannickfox@gmail.com>
2016-10-17 22:08:17 -0500

    comments

--- convoSig.h
+++ convoSig.h
@@ -7,10 +7,12 @@ double frec = 10;
 double phi = 0;
 
 double sigSen (double t) {
+    // sine function
     return amp * sin(t * frec * 2 * M_PI + phi);
 }
 
 double *sample (int N, double T, double (*fcn)(double)) {
+    // waveform function sampler for simulating
     double *d_sig = malloc(sizeof(double) * N);
     for (int k = 0; k < N; k++) {
         d_sig[k] = fcn((double)k * T);
@@ -19,6 +21,7 @@ double *sample (int N, double T, double (*fcn)(double)) {
 }
 
 double *hannWindow (int N) {
+    // Discrete hann window function
     double *hann = (double *) malloc(N * sizeof(double));
     for (int n = 0; n < N; n++) {
         hann[n] = 0.5 * (1 - cos(
